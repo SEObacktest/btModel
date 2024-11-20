b6a9001a77390e9bd3a6a35fac317c0af5789d98
hanbo <litterpigger@gmail.com>
2024-11-20 15:55:36 +0800

    config update

---
+++ BackTrader/config.py
@@ -0,0 +1,16 @@
+import configparser
+import os
+import sys
+CFG_FL_PATH="../sys.cfg"
+USER_CFG_SECTION = "cerebro_config"
+class Config:
+    def __init__(self):
+        config=configparser.ConfigParser()
+    
+        if not os.path.exists(CFG_FL_PATH):
+            print("No configuration file (sys.cfg) found!")
+            sys.exit(1)
+        else:
+            config.read(CFG_FL_PATH)
+    
+        self.INIT_BALANCE=config.get(USER_CFG_SECTION,'init_balance')
